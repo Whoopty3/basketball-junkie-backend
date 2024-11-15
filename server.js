585d70023ab2f636f2c392a72822c651ce54cf85
Payne Whitlock <paynewhitlock@Paynes-MBP-3.lan>
2024-11-14 22:23:23 -0500

    upload

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const cors = require('cors');
-
+const cors = require('cors'); // Make sure cors is imported
 
 const app = express();
-const port = process.env.PORT || 3001; // Use environment port or default to 3001
+const port = process.env.PORT || 3001;
 
-// Serve static files from the 'public' directory
+// Set up CORS to allow requests from your frontend domain
 app.use(cors({
-  origin: 'https://whoopty3.github.io'}));
+  origin: 'http://localhost:3000', // Update this URL to your frontend's local development URL
+}));
 
-// Route for serving players data at the root URL
+// Serve players.json data at the root URL
 app.get('/', (req, res) => {
-  fs.readFile(path.join(__dirname, 'players.json'), 'utf8', (err, data) => {
+  const playersFilePath = path.join(__dirname, 'players.json'); // Get the absolute path to players.json
+
+  fs.readFile(playersFilePath, 'utf8', (err, data) => {
     if (err) {
       console.error('Error reading players.json:', err);
       return res.status(500).json({ error: 'Failed to load players data' });
